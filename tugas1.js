67d95af7bf90b15fa27f5258fdb8b285c1c16601
Elitrirudiyati <71365611+Elitrirudiyati@users.noreply.github.com>
2020-09-19 15:51:23 +0700

    Add files via upload

---
+++ tugas1.js
@@ -0,0 +1,10 @@
+var namadepan = "Eli"
+function namabelakang(){
+	let namabelakang="rudiyati"
+	console.log(namabelakang)
+}
+const umur = 36
+
+console.log (namadepan)
+namabelakang();
+console.log(umur)
